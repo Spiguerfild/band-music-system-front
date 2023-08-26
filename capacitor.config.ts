4924d9d80f28ce848abc3fdab7cbd330d46c1101
Spiguerfild <luique16@outlook.com>
2023-08-25 23:05:04 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'band-music-system-front',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
