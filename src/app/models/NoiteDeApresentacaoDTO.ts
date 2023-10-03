56ca569f80955a6e9762aada52bd0a13068679d6
Spiguerfild <luique16@outlook.com>
2023-10-03 16:09:55 -0400

    edit escala puxando dados ja e detectando alteação

--- src/app/models/NoiteDeApresentacaoDTO.ts
+++ src/app/models/NoiteDeApresentacaoDTO.ts
@@ -2,7 +2,7 @@ import { BandaDTO } from "./BandaDTO";
 
 export interface NoiteDeApresentacaoDTO {
   id: number; // O tipo depende do tipo real de id em sua classe Java
-  data: string; // O tipo depende do tipo real de data em sua classe Java
+  data: number[]; // O tipo depende do tipo real de data em sua classe Java
   banda: BandaDTO;
 }
 
--- src/app/viewLeader/leaderEdits/edit-escala/edit-escala.page.ts
+++ src/app/viewLeader/leaderEdits/edit-escala/edit-escala.page.ts
@@ -1,101 +1,93 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
 import { BandaDTO } from 'src/app/models/BandaDTO';
 import { NoiteDeApresentacaoDTO } from 'src/app/models/NoiteDeApresentacaoDTO';
 import { bandaService } from 'src/app/services/domain/banda.service';
 import { noiteDeApresentacaoService } from 'src/app/services/domain/noiteDeApresentacao.service';
+
 @Component({
   selector: 'app-edit-escala',
   templateUrl: './edit-escala.page.html',
   styleUrls: ['./edit-escala.page.scss'],
 })
 export class EditEscalaPage implements OnInit {
-
   noites!: NoiteDeApresentacaoDTO[]; // array de todas as escalas
   bandas!: BandaDTO[]; // array de todas as bandas
-  noiteId!: number; // variavel para pegar o id da noite de apresentação (escala selecionada)
-  // selectedBandaId: number | null = null; // id da banda selecionada
+  noiteId!: number; // variável para pegar o ID da noite de apresentação (escala selecionada)
   selectedBandaDTO!: BandaDTO; // objeto da banda selecionada
-  isModalOpen = false; // variavel de controle do modal
-  selectChange = false; // variavel de controle do select
-  dateValue!: string; // valor da data 
+  isModalOpen = false; // variável de controle do modal
+  selectChange = false; // variável de controle do select
+
 
   constructor(
     private formBuilder: FormBuilder,
     private noiteDeApresentacaoService: noiteDeApresentacaoService,
     private bandaService: bandaService,
-    private alertController: AlertController,
-    private router: Router,
-    // private route: ActivatedRoute
+    private alertController: AlertController
   ) { }
 
-  /*Agrupa um formulário para estabelecer dominio e um conjunto de regras dentro desse dominio*/
   escalaForm!: FormGroup;
-
-  /*array de possiveis erros */
   errorsMessage!: string[];
 
-  /*Para acertar o id da banda coloque index +1 */
+  ngOnInit() {
+    this.escalaForm = this.formBuilder.group({
+      banda: ['', Validators.compose([Validators.required])],
+      data: ['', Validators.compose([Validators.required])],
+    });
+  }
+
   setOpen(isOpen: boolean, id: number) {
     this.isModalOpen = isOpen;
+
     if (isOpen === true) {
-      this.noiteId = id // pega a noite especifica
-      console.log('noite ID-->', this.noiteId)
+      this.noiteId = id;
       this.selectedBandaDTO = this.noites[this.noiteId - 1].banda;
 
-      // console.log('valorinterno', this.bandas[this.selectedBandaDTO.id - 1]) //valor no html
-
       this.noiteDeApresentacaoService.findById(id).subscribe(response => {
-        this.dateValue = response.data.toString().split('').reverse().join('')
 
+        console.log(response.data)
         this.escalaForm = this.formBuilder.group({
           id: [response.id, Validators.required],
           banda: [response.banda, Validators.required],
-          data: [response.data, Validators.required],
-          // data: [response.data.reverse().toString(), Validators.required],
-        })
-        console.log('resps==', response)
-        console.log('STRING DATA ====>', response.data)
+          data: [this.formatarData(response.data), [Validators.required]],
+        });
 
-      })
-      this.selectChange = false;
+        this.selectChange = false;
+      });
     }
   }
 
   ionDateChange() {
     this.selectChange = true;
   }
-  ionSelectChange() { /*se o select mudar ele muda o botão para ativar a alteração*/
+
+  ionSelectChange() {
     this.selectChange = true;
   }
 
   submit(noiteId: number) {
-
     if (this.escalaForm.invalid || this.escalaForm.pending) {
       return;
     }
 
-    this.errorsMessage = []; // edfine o array de erros como vazio
-    console.log('escalaform-->', this.escalaForm.value)
+    this.errorsMessage = [];
+
     this.noiteDeApresentacaoService.update(noiteId, this.escalaForm.value)
       .subscribe(response => {
         this.presentAlert('Sucesso', 'Banda alterada',
           `De:  ${this.selectedBandaDTO.nome}
           Para:${this.escalaForm.value.banda.nome}`,
           ['Ok']);
-        this.router.navigate(['/edit-general']);
-      })
+      });
 
     this.isModalOpen = false;
   }
-  //------------------------------------------------------------------------
+
   ionViewDidEnter() {
     this.noiteDeApresentacaoService.findAll()
       .subscribe(response => {
         this.noites = response;
-        console.log(response);
       }, error => {
         console.log(error);
       });
@@ -103,23 +95,13 @@ export class EditEscalaPage implements OnInit {
     this.bandaService.findAll()
       .subscribe(response => {
         this.bandas = response;
-        console.log(response);
       }, error => {
         console.log(error);
       });
-
   }
 
-  ngOnInit() {
-
-    this.escalaForm = this.formBuilder.group({
-      banda: ['', Validators.compose([Validators.required])],
-      data: ['', Validators.compose([Validators.required])],
-    })
-  }
-  //--------------------------------------------------------------------------------------
   async presentAlert(header: string, subHeader: string,
-    message: string, buttons: string[],) {
+    message: string, buttons: string[]) {
     const alert = await this.alertController.create({
       header,
       subHeader,
@@ -128,4 +110,25 @@ export class EditEscalaPage implements OnInit {
     });
     await alert.present();
   }
+
+  formatarData(arrayData: number[]): string {
+    // Extrai os elementos do array
+    const [ano, mes, dia] = arrayData;
+
+    // Formata o ano, mês e dia com zeros à esquerda, se necessário
+    const anoFormatado = ano.toString();
+    const mesFormatado = mes < 10 ? `0${mes}` : mes.toString();
+    const diaFormatado = dia < 10 ? `0${dia}` : dia.toString();
+
+    // Concatena os elementos formatados com "-" para formar a data
+    const dataFormatada = `${anoFormatado}-${mesFormatado}-${diaFormatado}`;
+
+    return dataFormatada;
+  }
+
+
+
+
+
+
 }
