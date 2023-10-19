b92fec6d704aff6787060ade5b1841541fd34566
Spiguerfild <luique16@outlook.com>
2023-10-18 22:21:58 -0400

    ⚡️ finalizando usico instrumento crud (insert só)

--- src/app/viewLeader/musicoInstrumento/add-edit-musico-instrumento/add-edit-musico-instrumento.page.ts
+++ src/app/viewLeader/musicoInstrumento/add-edit-musico-instrumento/add-edit-musico-instrumento.page.ts
@@ -2,7 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
-import { MusicaService } from 'src/app/services/domain/musica.service';
+import { InstrumentoDTO } from 'src/app/models/InstrumentoDTO';
+import { MusicoDTO } from 'src/app/models/MusicoDTO';
+import { instrumentoService } from 'src/app/services/domain/instrumento.service';
+import { MusicoService } from 'src/app/services/domain/musico.service';
+import { musicoInstrumentoService } from 'src/app/services/domain/musicoInstrumento.service';
 
 @Component({
   selector: 'app-add-edit-musico-instrumento',
@@ -10,18 +14,44 @@ import { MusicaService } from 'src/app/services/domain/musica.service';
   styleUrls: ['./add-edit-musico-instrumento.page.scss'],
 })
 export class AddEditMusicoInstrumentoPage implements OnInit {
-
-  musicaForm!: FormGroup;
+  musicos!: MusicoDTO[];
+  instrumentos!: InstrumentoDTO[];
+  musicoInstrumentoForm!: FormGroup;
   nomeBtn = '';
   cadOrAlt!: boolean;
   modoEdit = false;
+  musicoNome: string = '';
+  instrumentoNome: string = '';
   constructor(private route: ActivatedRoute,
     private formBuilder: FormBuilder,
-    public musicaService: MusicaService,
+    public musicoInstrumentoService: musicoInstrumentoService,
+    public musicoService: MusicoService,
+    public instrumentoService: instrumentoService,
     private alertController: AlertController,
     private router: Router,
 
   ) { }
+
+  ionViewDidEnter() {
+    this.musicoService.findAll()
+      .subscribe(response => {
+        this.musicos = response;
+        console.log(response);
+      }, error => {
+        console.log(error);
+      });
+
+    this.instrumentoService.findAll()
+      .subscribe(response => {
+        this.instrumentos = response;
+        console.log(response);
+      }, error => {
+        console.log(error);
+      });
+
+
+  }
+
 
   ngOnInit() {
     const id: number = Number(this.route.snapshot.paramMap.get('id'));
@@ -31,39 +61,42 @@ export class AddEditMusicoInstrumentoPage implements OnInit {
       this.nomeBtn = 'Alterar';
       this.cadOrAlt = true;
       this.modoEdit = true
-      this.musicaService.findById(id).subscribe(response => {
-        this.musicaForm = this.formBuilder.group({
+      this.musicoInstrumentoService.findById(id).subscribe(response => {
+        this.musicoNome = response.musico.nome
+        this.instrumentoNome = response.instrumento.nome
+        this.musicoInstrumentoForm = this.formBuilder.group({
           id: [response.id],
-          nome: [response.nome, Validators.compose([Validators.required, Validators.minLength(2), Validators.maxLength(30)])],
-          autor: [response.autor, Validators.compose([Validators.required, Validators.minLength(2), Validators.maxLength(30)])],
+          musico: [response.musico, Validators.required],
+          instrumento: [response.instrumento, Validators.required],
         })
       })
     } else {
       this.nomeBtn = 'Cadastrar';
       this.cadOrAlt = false;
-      this.musicaForm = this.formBuilder.group({
-        id,
-        nome: ['', Validators.compose([Validators.required, Validators.minLength(2), Validators.maxLength(30)])],
-        autor: ['', Validators.compose([Validators.required, Validators.minLength(2), Validators.maxLength(30)])],
+      this.musicoInstrumentoForm = this.formBuilder.group({
+        // id: [id],
+        musico: ['', [Validators.required]],
+        instrumento: ['', [Validators.required]],
       })
     }
   }
 
   submit() {
-    if (this.musicaForm.invalid || this.musicaForm.pending) {
+    if (this.musicoInstrumentoForm.invalid || this.musicoInstrumentoForm.pending) {
       return;
     }
 
     const id: number = Number(this.route.snapshot.paramMap.get('id'));
 
     if (this.cadOrAlt) {
-      this.musicaService.update(id, this.musicaForm.value)
+      this.musicoInstrumentoService.update(id, this.musicoInstrumentoForm.value)
         .subscribe(response => {
           this.presentAlert('Sucesso', 'Musica alterada',
             'Os dados foram alterados com sucesso', ['Ok',]);
         })
     } else {
-      this.musicaService.insert(this.musicaForm.value)
+      console.log('sub cad --->', this.musicoInstrumentoForm.value)
+      this.musicoInstrumentoService.insert(this.musicoInstrumentoForm.value)
         .subscribe(response => {
           this.presentAlert('Sucesso', 'Musica salva',
             'Os dados foram salvos com sucesso', ['Ok',]);
@@ -89,7 +122,7 @@ export class AddEditMusicoInstrumentoPage implements OnInit {
           handler: () => {
             // Executar ação de exclusão aqui.
             const id: number = Number(this.route.snapshot.paramMap.get('id'));
-            this.musicaService.delete(id).subscribe(
+            this.musicoInstrumentoService.delete(id).subscribe(
               () => {
                 // Banda excluída com sucesso, você pode redirecionar para uma página diferente
                 // ou realizar alguma ação específica após a exclusão.
@@ -120,7 +153,7 @@ export class AddEditMusicoInstrumentoPage implements OnInit {
     });
 
     await alert.present();
-    this.router.navigate(['sel-musica'])
+    this.router.navigate(['sel-musico-instrumento'])
   }
 
   backPage(rota: string) { // função que volta pra uma pagina especifica
