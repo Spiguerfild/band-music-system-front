45c204e2008394493fa21bbe3a41d334394f25ab
Spiguerfild <luique16@outlook.com>
2023-11-23 12:08:50 -0400

    💸 ✨ ⚡️ FINALIZADO PARA A APRESENTAÇÃO

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -1,6 +1,6 @@
-// export const API_CONFIG = {
-//   baseUrl: "http://localhost:8080"
-// }
 export const API_CONFIG = {
-  baseUrl: "http://200.53.30.36:8080/band-music"
+  baseUrl: "http://localhost:8080"
 }
+// export const API_CONFIG = {
+//   baseUrl: "http://200.53.30.36:8080/band-music"
+// }
